f264cdeb0005ee69d53398ce63b18fb0ac04ba90
Allef Sousa <allefsousa_1@hotmail.com>
2019-10-26 14:08:47 -0300

    Operações com banco de dados, Salvando e buscando dados

--- lib/helpers/contact_helper.dart
+++ lib/helpers/contact_helper.dart
@@ -1,10 +1,61 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+final String contactTable = "contactTable";
 final String idColumn = "idColumn";
 final String nameColumn = "nameColumn";
 final String emailColumn = "idemailColumn";
 final String phoneColumn = "phoneColumn";
 final String imgColumn = "imgColumn";
 
-class ContactHelper {}
+class ContactHelper {
+  static final ContactHelper _instace = ContactHelper.internal();
+
+  factory ContactHelper() => _instace;
+
+  ContactHelper.internal();
+
+  Database _db;
+
+  Future<Database> get db async {
+    if (db != null) {
+      return _db;
+    } else {
+      _db = await initDb();
+      return _db;
+    }
+  }
+
+  Future<Database> initDb() async {
+    final databasePatch = await getDatabasesPath();
+    final path = join(databasePatch, "contacts.db");
+    return await openDatabase(path, version: 1,
+        onCreate: (Database db, int newerVersion) async {
+      await db.execute(
+          "CREATE TABLE $contactTable($idColumn INTEGER PRIMARY KEY, $nameColumn TEXT, $emailColumn TEXT, $phoneColumn TEXT, $imgColumn TEXT)");
+    });
+  }
+   // salvando os dados em um BD
+  Future<Contact> saveContact(Contact contact) async{
+    Database dbContact = await db;
+    contact.id = await dbContact.insert(contactTable, contact.toMap());
+    return contact;
+  }
+
+  // busncando um contato pelo id
+  Future<Contact> getContact(int id) async {
+    Database dbContact = await db;
+    List<Map> maps = await dbContact.query(contactTable,columns: [idColumn,
+      nameColumn,emailColumn,phoneColumn,imgColumn], where: "$idColumn = ?", whereArgs: [id]);
+    if (maps.length > 0){
+      return  Contact.fromMap(maps.first);
+    }else{
+      return null;
+    }
+  }
+
+
+}
 
 class Contact {
   int id;
