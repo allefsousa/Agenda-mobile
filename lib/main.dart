08f21d503a90a3d90c0dc1d2ed235934a397ea03
Allef Sousa <allefsousa_1@hotmail.com>
2019-11-05 13:18:03 -0300

    Criando estrutura para receber um obejeto de outra tela

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app/ui/HomePage.dart';
+import 'package:flutter_app/ui/contact_page.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: HomePage(),
+    home: ContactPage(),
   ));
 }
 
--- lib/ui/contact_page.dart
+++ lib/ui/contact_page.dart
@@ -1,27 +1,39 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_app/helpers/contact_helper.dart';
-class ContactPage extends StatefulWidget {
 
+class ContactPage extends StatefulWidget {
   final Contact contact;
+
   ContactPage({this.contact});
 
   @override
   _ContactPageState createState() => _ContactPageState();
 }
 
-class _ContactPageState extends State<ContactPage> with SingleTickerProviderStateMixin {
+class _ContactPageState extends State<ContactPage>{
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _phoneController = TextEditingController();
+
 
   Contact _editContact;
+  bool _userEdited = false;
+
+
   @override
   void initState() {
     super.initState();
-    if(widget.contact == null){
+    if (widget.contact == null) {
       _editContact = Contact();
-    }else{
+    } else {
       _editContact = Contact.fromMap(widget.contact.toMap());
+      _nameController.text = _editContact.name;
+      _emailController.text = _editContact.email;
+      _phoneController.text = _editContact.phone;
     }
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -32,9 +44,54 @@ class _ContactPageState extends State<ContactPage> with SingleTickerProviderStat
         centerTitle: true,
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){},
+        onPressed: () {},
         child: Icon(Icons.save),
         backgroundColor: Colors.red,
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(10.0),
+        child: Column(
+          children: <Widget>[
+            GestureDetector(
+              child:  Container(
+                width: 140.0,
+                height: 140.0,
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(image: _editContact.img != null ?
+                    FileImage(File(_editContact.img)) : AssetImage("images/person.png")
+                    )
+                ),
+              ),
+            ),
+            TextField(
+              controller: _nameController,
+              decoration: InputDecoration(labelText: "Nome"),
+              onChanged: (text){
+                _userEdited = true;
+                setState(() {
+                  _editContact.name = text;
+                });
+              },
+            ),
+            TextField(
+              controller: _emailController,
+              decoration: InputDecoration(labelText: "Email"),
+              onChanged: (text){
+                _editContact.email = text;
+              },
+              keyboardType: TextInputType.emailAddress,
+            ),
+            TextField(
+              controller: _phoneController,
+              decoration: InputDecoration(labelText: "Phone"),
+              onChanged: (text){
+                _editContact.phone = text;
+              },
+              keyboardType: TextInputType.phone,
+            )
+          ],
+        ),
       ),
     );
   }
